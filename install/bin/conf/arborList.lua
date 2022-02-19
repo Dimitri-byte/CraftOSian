cd8e5233f87766a35e3ab1342135daaecfd277d1
Dimitri-byte <59877223+Dimitri-byte@users.noreply.github.com>
2022-02-19 17:34:48 +0100

    Create arborList.lua

---
+++ install/bin/conf/arborList.lua
@@ -0,0 +1,15 @@
+-- Liste des répertoires
+-- Utiliser pour créer ou supprimer l'OS
+arborList = {
+'/bin',
+'/boot',
+'/dev',
+'/etc',
+'/home',
+'/opt',
+'/tmp',
+'/usr/bin',
+'/usr',
+'/var/log',
+'/var',
+}
